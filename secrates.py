a044f7cc705efedafd89faa581a9359c7449e248
muhammad ingar <muhammadingar05@gmail.com>
2024-07-31 21:56:46 +0100

    0.0.1

--- secrates.py
+++ secrates.py
@@ -1,2 +1,3 @@
-databade_url = "mysql://root:ieTvKYsZfpzJOUQWAlGRoneGaEymYuuu@mysql.railway.internal:3306/railway"
+
+databade_url = "mysql://root:ieTvKYsZfpzJOUQWAlGRoneGaEymYuuu@monorail.proxy.rlwy.net:42550/railway"
 databade_ur ="mysql://root:ieTvKYsZfpzJOUQWAlGRoneGaEymYuuu@mysql.railway.internal:3306/railway"
