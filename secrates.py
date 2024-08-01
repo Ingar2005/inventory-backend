de846724b45f1fee88e76ea454ac210b9c725c2f
ingar <97912462+Ingar2005@users.noreply.github.com>
2024-08-01 10:37:07 +0100

    Update secrates.py

--- secrates.py
+++ secrates.py
@@ -1,3 +1,1 @@
-
-databade_url = "mysql://root:ieTvKYsZfpzJOUQWAlGRoneGaEymYuuu@monorail.proxy.rlwy.net:42550/railway"
-databade_ur ="mysql://root:ieTvKYsZfpzJOUQWAlGRoneGaEymYuuu@mysql.railway.internal:3306/railway"
+NOT IN USE
