268c82fcad708d0a66cbb4519ad42974c22dbf45
muhammad ingar <muhammadingar05@gmail.com>
2024-08-11 18:38:57 +0100

    change DB

--- config.py
+++ config.py
@@ -3,10 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_cors import CORS
 import os
+
+
 app = Flask(__name__)
 CORS(app)
-database_url = os.environ.get("DATABASE_URL")
-app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+database_url = os.environ.get("JAWSDB_URL")
+app.config["SQLALCHEMY_DATABASE_URI"]=database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =  False
 
 db = SQLAlchemy(app)
