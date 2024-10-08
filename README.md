c587e3a3b65a42ee1b452483950bbf42f689d17b
ingar2005 <muhammadingar05@gmail.com>
2024-10-08 11:45:14 +0100

    tryl

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-use environment veriable DATABASE_URL for the url 
+use environment veriable DB_USERNAME DB_PASSWORD DB_HOST DB_NAME in constraints of the format
+
+mysql://{user}:{password}@{host}:42550/{database_name}
--- config.py
+++ config.py
@@ -3,13 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_cors import CORS
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.pool import QueuePool
+from sqlalchemy.orm import  sessionmaker
 
-
+user  = os.environ.get("DB_USERNAME")
+password = os.environ.get("DB_PASSWORD")
+host = os.environ.get("DB_HOST")
+database_name = os.environ.get("DB_NAME")
+# print(f"mysql://{user}:{password}@{host}:3306/{database_name}")
+# engine = create_engine(
+#     f"mysql://{user}:{password}@{host}:3306/{database_name}",
+#     poolclass=QueuePool,
+#     pool_size=5,
+#     max_overflow=10
+# )
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 app = Flask(__name__)
 CORS(app)
-database_url = os.environ.get("JAWSDB_URL")
-app.config["SQLALCHEMY_DATABASE_URI"]=database_url
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =  False
+app.config["SQLALCHEMY_DATABASE_URI"]=f"mysql://{user}:{password}@{host}:42550/{database_name}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# Enable connection pooling
+app.config['SQLALCHEMY_POOL_SIZE'] = 20  # Adjust as needed
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20  # Adjust as needed
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600
+db= SQLAlchemy(app)
 
-db = SQLAlchemy(app)
 app.app_context().push()
